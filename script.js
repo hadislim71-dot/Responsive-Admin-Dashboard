370bf2162658ce3b69a9dfefa3a096525016b425
Hadi <hadislim71@gmail.com>
2026-09-02 22:35:02 +0300

    Finish Task 5

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementById("menuButton");
+
+const sidebar = document.getElementById("sidebar");
+
+
+menuButton.addEventListener("click", function() {
+
+    sidebar.classList.toggle("show");
+
+});
